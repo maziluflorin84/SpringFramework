aa5345c0511664573939f6200a5c7964e1918e27
Florin Mazilu <maziluflorin84@outlook.com>
2023-08-15 12:18:53 +0300

    Update, delete and select

--- springjdbc/src/main/java/com/trainings/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
+++ springjdbc/src/main/java/com/trainings/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
@@ -1,8 +1,11 @@
 package com.trainings.spring.springjdbc.employee.dao.impl;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.trainings.spring.springjdbc.employee.dao.EmployeeDao;
+import com.trainings.spring.springjdbc.employee.dao.rowmapper.EmployeeRowMapper;
 import com.trainings.spring.springjdbc.employee.dto.Employee;
 
 public class EmployeeDaoImpl implements EmployeeDao {
@@ -16,6 +19,36 @@ public class EmployeeDaoImpl implements EmployeeDao {
 		return result;
 	}
 
+	@Override
+	public int update(Employee employee) {
+		String sql = "update employee set firstname=?, lastname=? where id=?";
+		int result = jdbcTemplate.update(sql, employee.getFirstName(), employee.getLastName(), employee.getId());
+		return result;
+	}
+
+	@Override
+	public int delete(int id) {
+		String sql = "delete from employee where id=?";
+		int result = jdbcTemplate.update(sql, id);
+		return result;
+	}
+	
+	@Override
+	public Employee read(int id) {
+		String sql = "select * from employee where id=?";
+		EmployeeRowMapper rowMapper = new EmployeeRowMapper();
+		Employee employee = jdbcTemplate.queryForObject(sql, rowMapper, id);
+		return employee;
+	}
+	
+	@Override
+	public List<Employee> readList() {
+		String sql = "select * from employee";
+		EmployeeRowMapper rowMapper = new EmployeeRowMapper();
+		List<Employee> list = jdbcTemplate.query(sql, rowMapper);
+		return list;
+	}
+
 	public JdbcTemplate getJdbcTemplate() {
 		return jdbcTemplate;
 	}
@@ -23,5 +56,4 @@ public class EmployeeDaoImpl implements EmployeeDao {
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-
 }
--- springjdbc/src/main/java/com/trainings/spring/springjdbc/employee/test/Test.java
+++ springjdbc/src/main/java/com/trainings/spring/springjdbc/employee/test/Test.java
@@ -1,7 +1,11 @@
 package com.trainings.spring.springjdbc.employee.test;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.trainings.spring.springjdbc.employee.dao.EmployeeDao;
 import com.trainings.spring.springjdbc.employee.dto.Employee;
 
@@ -14,13 +18,43 @@ public class Test {
 		EmployeeDao dao = (EmployeeDao) context.getBean("employeeDao");
 		Employee employee = new Employee();
 		employee.setId(2);
-		employee.setFirstName("John");
-		employee.setLastName("Doe");
-		int result = dao.create(employee);
-		if (result == 1) {
-			System.out.println("Record inserted!!!");
-		} else {
-			System.out.println("Something went wrong!");
-		}
+		int result = 0;
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter:\n1 to add John Doe\n2 to rename John Doe\n3 to delete someone");
+			int input = scanner.nextInt();
+			if (input == 1) {
+				employee.setFirstName("John");
+				employee.setLastName("Doe");
+				result = dao.create(employee);
+				if (result == 1)
+					System.out.println("Record inserted!!!");
+			} else if (input == 2) {
+				employee.setFirstName("Johny");
+				employee.setLastName("Does");
+				result = dao.update(employee);
+				if (result == 1)
+					System.out.println("Record modified!!!");
+			} else if (input == 3) {
+				List<Employee> list = dao.readList();
+				System.out.println("Select an id from the list bellow\n");
+				System.out.println("Id\tFirst Name\tLast Name");
+				for (Employee item : list) {
+					System.out.println(item.getId() + "\t" + item.getFirstName() + "\t\t" + item.getLastName());
+				}
+				input = scanner.nextInt();
+				result = dao.delete(input);
+				if (result == 1) {
+					for (Employee item : list) {
+						if (item.getId() == input) {
+							System.out.println(item.getFirstName() + " was deleted!!!");
+						}
+					}
+				}
+			}
+			if (result != 1) {
+				System.out.println("Something went wrong");
+			}
+		} 
 	}
 }
