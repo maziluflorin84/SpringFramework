e022717968ca08e5e4037c7aa6a49dc460fc2dc2
Florin Mazilu <maziluflorin84@outlook.com>
2023-08-16 21:43:24 +0300

    Forgot to add RowMapper

---
+++ springjdbc/src/main/java/com/trainings/spring/springjdbc/employee/dao/rowmapper/EmployeeRowMapper.java
@@ -0,0 +1,20 @@
+package com.trainings.spring.springjdbc.employee.dao.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.trainings.spring.springjdbc.employee.dto.Employee;
+
+public class EmployeeRowMapper implements RowMapper<Employee> {
+
+	@Override
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Employee employee = new Employee();
+		employee.setId(rs.getInt(1));
+		employee.setFirstName(rs.getString(2));
+		employee.setLastName(rs.getString(3));
+		return employee;
+	}
+}
